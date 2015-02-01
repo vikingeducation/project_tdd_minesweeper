e6b8e9f8a0fa3a03cdcc94edc029a70e4c3650a0
Dan Berger <dsberger@gmail.com>
2015-02-01 15:48:32 -0800

    refactored Minesweeper game to abstract and encapsulate game logic

--- lib/minesweeper.rb
+++ lib/minesweeper.rb
@@ -4,13 +4,18 @@ require_relative 'turn'
 class Minesweeper
   def initialize
     @mines = Minefield.new
-    print "welcome to\nMINESWEEPER\n\nCOMMANDS:\n'C' to clear\n'A' to auto-clear\n'F' to flag\n'U' to un-flag\nThe ⚑ shows you how many flags are left"
+    intro
     loop do
       print_game_board
-      turn = Turn.new
-      @mines.take_turn(turn.message)
-      game_over_sequence if @mines.game_over?
+      take_a_turn
+      check_for_game_end
     end
+  end
+
+  private
+
+  def intro
+    print "welcome to\nMINESWEEPER\n\nCOMMANDS:\n'C' to clear\n'A' to auto-clear\n'F' to flag\n'U' to un-flag\nThe ⚑ shows you how many flags are left"
   end
 
   def print_game_board
@@ -18,16 +23,33 @@ class Minesweeper
     @mines.render
   end
 
+  def take_a_turn
+    turn = Turn.new
+    @mines.take_turn(turn.message)
+  end
+
+  def check_for_game_end
+    game_over_sequence if @mines.game_over?
+  end
+
   def game_over_sequence
     if @mines.won?
-      @mines.flag_remaining_mines
-      print_game_board
-      puts "CONGRATULATIONS!"
-    elsif @mines.lost?
-      @mines.blow_up_board
-      print_game_board
-      puts "BOOM! YOU LOST."
+      winning_sequence
+    else
+      losing_sequence
     end
     exit
+  end
+
+  def winning_sequence
+    @mines.flag_remaining_mines
+    print_game_board
+    puts "CONGRATULATIONS!"
+  end
+
+  def losing_sequence
+    @mines.blow_up_board
+    print_game_board
+    puts "BOOM! YOU LOST."
   end
 end
