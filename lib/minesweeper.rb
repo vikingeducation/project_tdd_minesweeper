b24d14b33a9ea92d262235fa8149b859e7333c4f
Daniel Au <dakmau@gmail.com>
2015-02-01 00:19:06 -0500

    Fix bugs, add game logic and player logic

--- lib/minesweeper.rb
+++ lib/minesweeper.rb
@@ -1,3 +1,44 @@
+require 'pry'
+
+class Minesweeper
+  def initialize
+    @game_board = Board.new
+    @player = Player.new
+    @player_wants_to_continue = true
+    play
+  end
+
+  def play
+    while player_wants_to_continue
+      game_board.select_coordinate(get_user_input)
+      exit if player_has_lost || player_has_won
+    end
+  end
+
+
+  def player_has_lost?
+    #Condition
+    puts "Congratulations!"
+  end
+
+  def player_has_won?
+    #Condition
+    puts "Congratulations!"
+  end
+end
+
+class Player
+  def get_user_input
+    print ("Enter your move: ")
+    player_choice = gets.chomp
+    if player_choice.upcase == "Q"
+      @player_wants_to_continue = false
+      exit
+    end
+    player_choice = player_choice.split(",").map(&:to_i)
+  end
+end
+
 class Cell
   attr_accessor :mine, :hidden, :flagged, :adjacent_mines
   def initialize(mine=false, hidden=true, flagged=false, adjacent_mines=0)
@@ -6,14 +47,14 @@ class Cell
     @flagged = flagged
     @adjacent_mines = adjacent_mines
   end
-  
+
   def symbolize
     if (@flagged)
-      return "⚑" #change to flag under dr au
-     elsif(@hidden)
+      return "⚑"
+    elsif(@hidden)
       return "■"
-    elsif(@mine) #Switch later
-      return "✷"  
+    elsif(@mine)
+      return "✷"
     elsif(@adjacent_mines == 0)
       return "□"
     elsif(@adjacent_mines)
@@ -29,20 +70,23 @@ class Board
     @board = Array.new(@size) {Array.new(@size){Cell.new}}
     # place_mines  #bring back later
   end
-  
+
   def generate_mines
     (0..99).to_a.sample(10)
   end
-  
-  def place_mines mine_locations #comment out later
-  #  mine_locations = generate_mines
+
+  def place_mines mine_locations
     mine_locations.each do |x|
       tens = x / 10
       ones = x % 10
       @board[tens][ones].mine = true
     end
   end
-  
+
+  def initialize_mines
+    place_mines(generate_mines)
+  end
+
   def set_all_cell_adjacent_mines
     @board.each_with_index do |row, row_index|
       @board[row_index].each_with_index do |column, column_index|
@@ -50,11 +94,11 @@ class Board
       end
     end
   end
-  
+
   def is_mine? (row, column)
     @board[row][column].mine
   end
-  
+
   def set_cell_adjacent_mines(row, column)
     around_array = [-1, 0, 1]
     around_array.each do |row_offset|
@@ -65,7 +109,7 @@ class Board
       end
     end
   end
-  
+
   def is_in_bound? (row_index, column_index)
     if (row_index < 0 || column_index < 0)
       return false
@@ -75,11 +119,11 @@ class Board
       return true
     end
   end
-  
+
   def is_empty?(row,col)
     @board[row][col].mine == false
   end
-  
+
   def autoclear(row, column)
     around_array = [-1, 0, 1]
     around_array.each do |row_offset|
@@ -97,7 +141,7 @@ class Board
       end
     end
   end
-  
+
   def check_victory?
     @board.flatten.each do |cell|
       if cell.mine == false && cell.hidden == true
@@ -112,9 +156,9 @@ class Board
       if cell.mine == true && cell.hidden == false
         return true
       end
-    end    
+    end
   end
-  
+
   def render
     print "   "
     10.times do |column_number|
@@ -142,21 +186,20 @@ class Board
     end
     print "\n"
   end
+
+  def select_coordinate
+  end
+
 end
 
-class Minesweeper
-  def move(ary)
-    
-    check_victory?
-    check_loss?
-  end
-end
 
 myBoard = Board.new
 # myBoard.render
-myBoard.place_mines([50,51,52,53,54,55,56,57,58,59])
+myBoard.place_mines([0,3,20,40,55,33,34,35,60,70])
+myBoard.set_all_cell_adjacent_mines
 # myBoard.set_all_cell_adjacent_mines
 myBoard.autoclear(7,5)
+
 myBoard.render
 
 
