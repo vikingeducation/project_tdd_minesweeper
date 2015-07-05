8f8de2090551142c1e6ac3047fe8c0923e42c10e
alokp <alokpra@gmail.com>
2015-07-04 17:23:02 -0700

    tested upto loss

--- lib/board.rb
+++ lib/board.rb
@@ -4,12 +4,28 @@ class Board
 	attr_reader :game_board, :mines
 
 	def initialize(size = 10, mines = 9)
+		@mines = mines
+		create_game_board(size)
+		set_mines(mines)
+		return nil
+	end
+
+	def create_game_board(size)
 		@game_board = []
 		for i in 0..((size**2)-1) do
-			@game_board << Cell.new(:"#{i}")
+			@game_board << Cell.new("#{i}") #changed from symbold to string just so I could test player can change the state of a square while avoiding mines
 		end
-		@mines = mines
-		return nil
+	end
+
+	def set_mines(no_of_mines)
+		remaining_mines_to_set = no_of_mines
+		while remaining_mines_to_set > 0
+			square_to_plant_mine = @game_board.sample
+			if square_to_plant_mine.mine == 0
+				square_to_plant_mine.set_mine
+				remaining_mines_to_set -= 1
+			end
+		end
 	end
 
 	def show_remaining_flags
@@ -23,37 +39,44 @@ class Board
 	end
 
 	def change_state_of_square(square_to_change)
-		@game_board[square_to_change].state_change
+		@game_board[square_to_change].check_for_mine? ? game_over : @game_board[square_to_change].state_change
 	end
 
-	def render
+	def game_over
+		puts "You lost!"
+	end
+
+	def render_grid
 		size_of_side = (@game_board.size)**(1.0/2)
 		count = 0
-		selected_squares = 0
+		num_of_selected_squares = 0
 		while count < @game_board.flatten.size
 			count_in_row = 0
 			print ("Row #{(count/size_of_side + 1).to_i}").ljust(10)		#http://www.unicode.org/charts/beta/nameslist/n_25A0.html
 			while count_in_row < size_of_side do
-				#print "#{count + count_in_row}"
-				if @game_board[count + count_in_row].state == 1
-					print "\u2B1C "
-					selected_squares += 1
-				elsif @game_board[count + count_in_row].state == 0
-					print "\u2B1B "
-				end
+				num_of_selected_squares += render_each_square(count + count_in_row)
 				count_in_row += 1
 			end
 			puts ""
 			count += size_of_side
 		end
-		return selected_squares  #returning only to make test pass
+		return num_of_selected_squares  #returning only to make test pass
 	end
 
+	def render_each_square(position_in_grid)
+		square_has_been_selected_test = 0
+		if @game_board[position_in_grid].state == 1
+			print "\u2B1C "
+			square_has_been_selected_test += 1
+		elsif @game_board[position_in_grid].state == 0
+			print "\u2B1B "
+		end
+		return square_has_been_selected_test
+	end
 end
 
-
 class Cell
-	attr_reader :position_in_grid, :state, :mine, :flag
+	attr_reader :position_in_grid, :state, :flag, :mine
 	def initialize(position_in_grid)
 		@position_in_grid = position_in_grid				# place of cell assigned by digit
 		@state = 0    	     								# 0 or 1 for selected or not
@@ -68,6 +91,14 @@ class Cell
 		else
 			@state = 0
 		end
+	end
+
+	def set_mine
+		@mine = 1
+	end
+
+	def check_for_mine?
+		@mine == 1 ? true : false
 	end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -17,25 +17,40 @@ describe Board do
 		end
 	end
 
+context "tests that need a cell without a mine" do
+	before (:each) do
+		@square = board.game_board.sample
+		while (@square.mine == 1)
+			@square = board.game_board.sample
+		end
+	end
+
 	describe '#change_state_of_square' do
 		it 'player can change the state of a square' do
-			board.change_state_of_square(5)
-			expect(board.game_board[5].state).to eq(1)
+			board.change_state_of_square(@square.position_in_grid.to_i)
+			expect(board.game_board[@square.position_in_grid.to_i].state).to eq(1)   #Q: how can I write this to avoid mines?
 		end
 
-		specify 'a state of the square should be cleared' do   #this is similar to, the board has two states
-			condition1 = board.game_board[3].state
-			board.change_state_of_square(3)
-			condition2 = board.game_board[3].state
+		specify 'a square can be cleared' do   #this is similar to, the board has two states
+			condition1 = board.game_board[@square.position_in_grid.to_i].state
+			board.change_state_of_square(@square.position_in_grid.to_i)
+			condition2 = board.game_board[@square.position_in_grid.to_i].state
 			expect(condition1).not_to eql(condition2)
 		end
 	end
 
 	specify 'on selection the square should appear as cleared in the render' do
-		condition1 = board.render
-		board.change_state_of_square(1)
-		condition2 = board.render
+		condition1 = board.render_grid  #Q:how do I stop the board from actually rendering in the test loop
+		board.change_state_of_square(@square.position_in_grid.to_i)
+		condition2 = board.render_grid
 		expect(condition2 - condition1).to eq(1)
+	end
+
+end
+
+	it "should end game if player selects a square with a mine in it" do
+		board.game_board[1].set_mine
+		expect(board.change_state_of_square(1)).to be_nil
 	end
 
 end
