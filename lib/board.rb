02607de5aa0b6edc02961af5fda8b0ea72cf3b22
Donald Kelsey <dmkelseyii@gmail.com>
2015-07-06 16:01:53 -0500

    Worked out properly generating hints

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,10 @@
 class Board
-  attr_accessor :field
+  attr_accessor :field, :size
 
 
   def initialize()
-  
     @field = Array.new(10) { Array.new(10) {{:hint => 0, :flag => false, :mine => false, :revealed =>false}}  }
-
+    @size = 10
     generate_mines
     @flag_count = 10
     @game_over = false
@@ -15,13 +14,43 @@ class Board
     @game_over
   end
 
+  # How to generate hints
+  # Take each square and count surrounding mines
+  def generate_hints
+    @field.each_with_index do |row, x|
+      row.each_with_index do |h, y|
+        getsq(x,y)[:hint] = get_nearby_mines(x,y)
+      end
+    end
+  end
+
+  def get_around(x, y)
+    map = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,1],[1,0],[1,-1]]
+
+    surrounding = []
+    map.each do |val|
+      if getsq(val[0] + x, val[1] + y)
+        surrounding << getsq(val[0] + x,val[1] + y)
+      end
+    end
+    return surrounding
+  end
+
+  def get_nearby_mines(x,y)
+    nearby = get_around(x, y)
+    nearby.select {|sq| sq[:mine]}.length
+  end
+
+
   def generate_mines
   count=0
     while count<9
-      if @field[rand(0..9)][rand(0..9)][:mine] == false
-        @field[rand(0..9)][rand(0..9)][:mine] = true  
+      row = rand(0..9)
+      col = rand(0..9)
+      unless @field[col][row][:mine]
+        @field[col][row][:mine] = true
         count += 1
-      end 
+      end
     end
   end
 
@@ -36,8 +65,13 @@ class Board
       getsq(x,y)[:revealed]=true
     end
   end
+
   def getsq(x,y)
-   @field[x][y]
+    unless x >= @size || x < 0 || y >= @size || y < 0
+      return @field[x][y]
+    else
+      return nil
+    end
   end
 
   def clear_square(player_move)
@@ -61,4 +95,3 @@ class Board
     @flag_count += 1
   end
 end
-  
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,8 +2,9 @@ require 'board'
 
 describe "Board" do
   let(:board){Board.new}
+  let(:new_board) {Array.new(10) { Array.new(10) {{:hint => 0, :flag => false, :mine => false, :revealed =>false}}  }}
   describe "#Initialize" do
-    
+
     it 'should create Board with 10 rows' do
       expect(board.field.size).to be(10)
     end
@@ -14,8 +15,8 @@ describe "Board" do
 
     it 'has 9 mines' do
       count=0
-      
-      board.field.each do |row| 
+
+      board.field.each do |row|
         row.each do |h|
           count+=1 if h[:mine]
         end
@@ -27,25 +28,50 @@ describe "Board" do
       expect(board.flag_count).to eq(10)
     end
 
-  describe "Moves on board"
+    it "should have hints for mines" do
+      board.field = new_board
+      board.field[1][1][:mine] = true
+      board.generate_hints
+      expect(board.field[0][0][:hint]).to eq(1)
+    end
 
-    it "flags square when flag" do
-      board.flag(1,1)
-      expect(board.field[1][1][:flag]).to be(true)
+    it "should properly count amount of mines for the hint" do
+      board.field = new_board
+      board.field[1][1][:mine] = true
+      board.field[1][0][:mine] = true
+      board.generate_hints
+      expect(board.field[0][0][:hint]).to eq(2)
+    end
+
+  end
+
+  describe "Moves on board" do
+
+    describe "#flag" do
+
+      it "flags square when flag" do
+        board.flag(1,1)
+        expect(board.field[1][1][:flag]).to be(true)
+      end
+
+      it "should not place flag if out of flags"
+
     end
 
 
     it "cleares the square when played" do
+      board.field[1][1][:mine] = false
       board.clear(1,1)
       expect(board.field[1][1][:revealed]).to be(true)
-
     end
 
-    it "check that game is over when mine is revealed" 
+    it "check that game is over when mine is revealed" do
+      board.field[1][1][:mine] = true
+      board.clear(1,1)
+      expect(board.game_over?).to be true
+    end
 
   end
- 
 
-  
 end
 
