dbc15b7047bd6a21b620ba31a46dda095f63b6e9
alokp <alokpra@gmail.com>
2015-07-06 15:08:52 -0700

    working on player class

--- lib/player.rb
+++ lib/player.rb
@@ -11,9 +11,25 @@ class Player
     @difficulty = selected_level[0]
   end
 
-  def select_move
-
+  def select_move(gameboard)
+    print "Enter move (row,col): "
+    move = gets.chomp.split ","
+    move.map {|col| col.to_i}
+    validate(move, gameboard)
   end
 
-
+  def validate?(move, gameboard)
+    row_size = gameboard.flatten.size**(1.0/2)
+    ((1..row_size+1).include? move[0]) && ((1..row_size+1).include? move[1]) ? true : false
+  end
 end
+
+
+    # if @difficulty == "B"
+    #    size = 5
+    # elsif @difficulty == "I"
+    #   size = 10
+    # else @difficulty == "A"
+    #   size = 20
+    # end
+    # return size
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,6 +4,7 @@ require 'minesweeper'
 describe Player do
 
   let(:player){Player.new}
+  let(:mock_gameboard){(0..100).to_a}
 
   describe "#initialize" do
 
@@ -18,12 +19,32 @@ describe Player do
 
   end
 
-  describe "#select_move" do
+  describe "#select_move and #valid_move" do
+
+    specify "player enters move" do
+      allow(player).to receive(:gets).and_return("1,2")
+      expect(player.select_move(mock_gameboard)).to be_a(Array)
+    end
+
+    specify "player move is validiated" do
+      # allow(player).to receive(:select_move).and_return([1,2])
+      # allow(player).to receive(:gameboard).with([1,2]).and_return((0..100).to_a)
+      # expect(player.validate).to be_truthy
+      expect(player.validate?([1,2], mock_gameboard)).to be_truthy
+    end
+
+    specify "if player move is invalid, ask again" do
+
+    end
+
+    specify "player move is converted to specific cell" do
+
+    end
 
   end
 
-  describe "#valid_move" do
+  # describe "#valid_move" do
 
-  end
+  # end
 
 end
