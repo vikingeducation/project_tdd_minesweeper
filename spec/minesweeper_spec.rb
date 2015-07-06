0d490a7bb81e8901223cdce86f92d3fa9bd31fff
michelletoussaint <toussaint.michelle@gmail.com>
2015-07-06 17:05:48 -0400

    Testing play method

--- spec/minesweeper_spec.rb
+++ spec/minesweeper_spec.rb
@@ -13,6 +13,39 @@ describe Minesweeper do
 
   end
 
+  describe "#play" do
+
+    before do
+      my_board = instance_double("Board", :render => nil)
+      my_player = instance_double("Player", :get_move => Tile.new(0,0))
+      subject.instance_variable_set(:@board, my_board)
+      subject.instance_variable_set(:@player, my_player)
+    end
+
+    it "calls the #print_instructions method" do
+
+      expect(subject).to receive(:print_instructions)
+      allow(subject).to receive(:render)
+      allow(subject).to receive(:get_move)
+      subject.play
+
+    end
+
+    it "loops until game over is true" do
+      # allow(subject).to receive(:player_win?).and_return(true)
+
+    end
+
+    specify "player.get_move return an instance of a tile" do
+
+
+    end
+
+
+
+
+  end
+
   describe "#game_over?" do
 
     let(:my_tile) {Tile.new(1,1)}
