3f6c07f40361660a7c3fcfcb84f901fd4bf4b2be
Sia <siakaramalegos@gmail.com>
2015-11-20 11:28:32 -0600

    Base board spec and lib files

---
+++ spec/board_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../lib/board.rb'
+
+describe Board do
+
+  describe '#initialize' do
+
+    it 'should be a Board'
+    it 'should be 10x10 by default'
+    it 'should have 9 mines by default'
+    it 'should show remaining flags'
+  end
+
+end
