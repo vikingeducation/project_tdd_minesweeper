2fb256e548cfbc69866c0f1f0181a9e2d0a54046
Sia <siakaramalegos@gmail.com>
2015-11-21 09:11:30 -0600

    Completes board initialize specs and lib

---
+++ lib/board.rb
@@ -0,0 +1,54 @@
+require 'pry'
+require 'rainbow'
+
+class Board
+  attr_reader :mines, :board, :remaining_flags, :visible_board, :size
+
+  def initialize(board=nil, visible_board=nil, mines=9, size=10)
+    @mines = mines
+    @remaining_flags = 9
+    @size = size
+
+    # 0 indicates no mine, 1 indicates mine
+    @board ||= create_board(size, mines)
+
+    # blank indicates not yet cleared
+    @visible_board ||= Array.new(size){ Array.new(size, ' ') }
+  end
+
+  def render
+    # TODO: delete this loop
+    @board.each do |row|
+      puts row.to_s
+    end
+
+    puts Rainbow("Remaining mines: ").black.bg(:yellow) + Rainbow("#{@remaining_flags}").red.bg(:yellow)
+
+    puts "    Column:"
+    print "    "
+    (1..@size).each {|c| print "  #{c} "}
+    puts "\nRow " + "-" * (@size * 4 + 1)
+
+    (@size-1).downto(0) do |row|
+      puts "#{(row+1).to_s.rjust(4)}| " + @visible_board[row].join(' | ') + ' |'
+        puts "    " + "-" * (@size * 4 + 1)
+    end
+  end
+
+  private
+
+  def create_board(size, mines)
+    @board = Array.new(size){ Array.new(size, 0) }
+    mine = 0
+    until mine == mines
+      rand_row = rand(0..9)
+      rand_col = rand(0..9)
+      if @board[rand_row][rand_col] == 0
+        @board[rand_row][rand_col] = 1
+        mine += 1
+      end
+    end
+    @board
+  end
+
+end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,12 +2,27 @@ require_relative '../lib/board.rb'
 
 describe Board do
 
+  let(:board){ Board.new }
+
   describe '#initialize' do
 
-    it 'should be a Board'
-    it 'should be 10x10 by default'
-    it 'should have 9 mines by default'
-    it 'should show remaining flags'
+    it 'should be a Board' do
+      expect(board).to be_a(Board)
+    end
+
+    it 'should be 10x10 by default' do
+      expect(board.board.size).to eq(10)
+      expect(board.board.select{|col| col.size == 10}.size).to eq(10)
+    end
+
+    it 'should have 9 mines by default' do
+      expect(board.mines).to eq(9)
+      expect(board.board.map{|i| i.inject(:+)}.inject(:+)).to eq(9)
+    end
+
+    it 'should show remaining flags' do
+      expect(board.remaining_flags).to eq(9)
+    end
   end
 
 end
