efa72d34468a81757b1fda1533ff0544b79fa0fc
Kit Langton <kitlangton@gmail.com>
2016-01-11 16:57:45 -0500

    passing to pair

--- test.rb
+++ test.rb
@@ -6,10 +6,54 @@ class Tile
     @mine = mine
   end
 
+  def place_mine
+    @mine = true
+  end
+
   def inspect
     @mine ? "M" : "0"
   end
 end
+
+class Board
+
+  NEIGHBOR_COORDS = [
+    [0,1],
+    [1,1],
+    [1,0],
+    [1,-1],
+    [0,-1],
+    [-1,-1],
+    [-1,0],
+    [-1,1]
+  ]
+
+  def initialize
+    @data = Array.new(10) { Array.new(10) { Tile.new } }
+  end
+
+  def neighbors_for_coord(x,y)
+    NEIGHBOR_COORDS.map do |xdiff, ydiff|
+      # [xdiff + x, ydiff + y]
+      neighbor_x = xdiff + x
+      neighbor_y = ydiff + y
+      self[neighbor_x, neighbor_y]
+    end
+  end
+
+  def [](x, y)
+    @data[y][x]
+  end
+end
+
+b = Board.new
+
+b[1, 3].place_mine # => true
+b[1, 3] # => M
+b[2, 3].place_mine # => true
+b[1, 2].place_mine # => true
+
+b.neighbors_for_coord(1, 3) # => [0, 0, M, 0, M, 0, 0, 0]
 
 @array_of_tiles = Array.new(10) { Array.new(10) { Tile.new } }
 
