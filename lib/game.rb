e0ce98705084c1dbd988aff279c6633ae103167d
Stephen Mayeux <stephenmayeux@gmail.com>
2016-05-14 12:58:00 +0900

    fix flag placement bug

--- lib/game.rb
+++ lib/game.rb
@@ -14,7 +14,7 @@ class Game
     puts "This 10x10 grid has 9 mines!\nEnter coordinates to clear mines and get clues.\nWrite 'F' after coordinates to add a flag on suspected mine.\nIf you clear a mine, you're dead!\n\n"
     loop do
       render_board
-      make_move(get_coordinates)
+      get_coordinates
     end
   end
 
@@ -25,7 +25,7 @@ class Game
         tile = @board.tiles[x][y]
         if tile.hidden
           print "|   "
-        elsif !tile.hidden
+        elsif tile.hidden == false
           if tile.mine == true
             print "| * "
           elsif tile.flag == true
@@ -47,14 +47,15 @@ class Game
 
   def process_input(input)
     coordinates = input.split
-    coordinates[0].to_i
-    coordinates[1].to_i
+    x = coordinates[0].to_i
+    y = coordinates[1].to_i
+    flag = coordinates[2]
+    make_move(x,y,flag)
   end
 
-  def make_move(coordinates)
-    x, y = coordinates[0], coordinates[1]
+  def make_move(x,y,flag)
     tile = @board.tiles[x][y]
-    if coordinates[2] == "F"
+    if flag == "F"
       tile.hidden = false
       tile.flag = true
       @flags -= 1
