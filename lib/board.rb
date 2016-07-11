460e7d3b2bffa0f96e07114b1e29e5d1a7d01af2
Joe Bernardi <joebernardi@hotmail.com>
2016-07-11 14:47:30 -0500

    add mine populate test

---
+++ lib/board.rb
@@ -0,0 +1,37 @@
+require 'pry'
+
+
+class Board
+
+
+	def initialize( board = nil )
+
+		@board = Array.new( 10 ) { Array.new( 10 ) }
+		@flags = 9
+		@mines = 9
+
+	end
+
+	def place_mines
+
+		count = @mines
+
+		until count < 0
+
+			row = rand( 0..9 )
+			col = rand( 0..9 )
+
+			if @board[ row ][ col ] != 'M'
+				@board[ row ][ col ] = 'M'
+				count -= 1
+			end
+
+		end
+
+		return @board
+
+	end
+
+
+
+end
---
+++ spec/board_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+require 'board'
+require 'render'
+
+describe '.Board' do
+
+	let( :board ) { Board.new }
+	let( :render ) { instance_double( "Render" ) }
+
+	describe '#initialize' do
+
+		it 'board should be a Board' do
+
+			expect( board ).to be_a( Board )
+
+		end
+
+		it 'should create a 10 x 10 array' do
+
+			expect( board.instance_variable_get( :@board )).to eq( Array.new(10) { Array.new( 10 )})
+
+		end
+
+		it 'should set @flags == 9' do
+
+			expect( board.instance_variable_get( :@flags )).to eq( 9 )
+
+		end
+
+
+		it 'should set @mines == 9' do
+
+			expect( board.instance_variable_get( :@mines )).to eq( 9 )
+
+		end
+
+	end #/.initialize
+
+	describe "#place_mines" do
+
+		it 'should populate the board with 10 mines' do
+
+
+			array = board.place_mines
+
+			count = 0
+			array.each do | x |
+
+				x.each do | y |
+				 count +=1 if y == 'M'
+
+				end
+
+			end
+
+			expect( count ).to eq( 10 )
+
+
+		end
+
+	end #/.place_mines
+
+
+end #/.Board
