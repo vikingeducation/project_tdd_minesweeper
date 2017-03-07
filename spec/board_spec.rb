47be7f165e23dd6f00f42faa9499ec18bc30d9ac
Roy Chen <roychenlei@gmail.com>
2017-03-07 13:48:41 +0800

    initial specs for Board class

---
+++ spec/board_spec.rb
@@ -0,0 +1,57 @@
+# spec/board_spec.rb
+
+describe "Board" do
+  describe "#initialize" do
+    it "sets up a 10 x 10 empty array"
+
+    it "sets the number of flags to 10"
+  end
+
+  describe "setup_minefield" do
+    it "fills up the array with Cells"
+
+    it "randomly sets 10 Cells to have mines"
+  end
+
+  describe "valid_coordinate?" do
+    it "returns true if the provided row and column are within range"
+
+    it "returns false if the provided row and column are not within range"
+
+    it "raises an error if the provided row and column inputs are not integers"
+  end
+
+  describe "num_adjacent_mines" do
+    it "returns 0 if the number of adjacent mines for the Cell is 0"
+
+    it "returns 1 if the number of adjacent mines for the Cell is 1"
+
+    it "returns 2 if the number of adjacent mines for the Cell is 2"
+
+    it "returns 3 if the number of adjacent mines for the Cell is 3"
+
+    it "returns 4 if the number of adjacent mines for the Cell is 4"
+
+    it "returns 5 if the number of adjacent mines for the Cell is 5"
+
+    it "returns 6 if the number of adjacent mines for the Cell is 6"
+
+    it "returns 7 if the number of adjacent mines for the Cell is 7"
+
+    it "returns 8 if the number of adjacent mines for the Cell is 8"
+  end
+
+  describe "flags_left?" do
+    it "returns true if the number of flags is greater than 0"
+
+    it "returns false otherwise"
+  end
+
+  describe "decrement_flags" do
+    it "decreases the number of flags remaining by 1"
+  end
+
+  describe "increment_flags" do
+    it "increases the number of flags remaining by 1"
+  end
+end
