fc366ecf2d5fb46728c893c0348f768f96e48275
Roy Chen <roychenlei@gmail.com>
2017-03-07 22:07:43 +0800

    modified initialize to create grid with all Cell objects

--- lib/board.rb
+++ lib/board.rb
@@ -1,15 +1,13 @@
+require_relative './cell.rb'
+
 module Minesweeper
   class Board
     attr_reader :grid,
                 :flags
 
     def initialize(grid = nil)
-      @grid = grid.nil? ? Array.new(10) { Array.new(10, nil) } : grid
+      @grid = grid.nil? ? Array.new(10) { Array.new(10, Cell.new) } : grid
       @flags = 9
-    end
-
-    # TODO: make this method private and implicitly test it
-    def setup_minefield
     end
   end
 end
