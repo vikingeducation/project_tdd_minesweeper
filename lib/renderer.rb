15ba8c367a6a9518cf9dbbff01c545115e1fc9f3
Roy Chen <roychenlei@gmail.com>
2017-03-16 14:04:30 +0800

    added the displaying of col/row numbers to the grid

--- lib/renderer.rb
+++ lib/renderer.rb
@@ -8,13 +8,18 @@ module Minesweeper
 
     def draw_grid(show_mines = false)
       grid = ""
-      
+      grid += column_numbers
+
+      row_num = 0
+
       board.grid.each do |row|
         row.each do |cell|
           cell.clear if (show_mines && cell.mine)
-          grid += cell.to_s + " "
+          grid += "#{cell.to_s} "
         end
-        grid += "\n"
+        
+        grid += "\t#{row_num}\n"
+        row_num += 1
       end
 
       puts grid
@@ -24,6 +29,18 @@ module Minesweeper
     def show_flags_left
       puts "Flags left: #{board.flags}"
       puts
+    end
+
+    private
+
+    def column_numbers
+      output = ""
+
+      board.rows.times do |row|
+        output += "#{row} "
+      end
+
+      output += "\n\n"
     end
   end
 end
--- spec/renderer_spec.rb
+++ spec/renderer_spec.rb
@@ -38,7 +38,7 @@ describe "Renderer" do
 
     it "correctly displays a board with all uncleared cells and no mines" do
       renderer.board = test_board
-      expected_output = /. . . \n. . . \n. . . \n/
+      expected_output = /0 1 2 \n\n. . . \t0\n. . . \t1\n. . . \t2\n/
       expect { renderer.draw_grid }.to output(expected_output).to_stdout
     end
 
@@ -51,7 +51,7 @@ describe "Renderer" do
 
       renderer.board.clear(1, 1)
 
-      expected_output = /. . . \n. 2 . \n. . . \n/
+      expected_output = /0 1 2 \n\n. . . \t0\n. 2 . \t1\n. . . \t2\n/
       expect { renderer.draw_grid }.to output(expected_output).to_stdout
     end
 
@@ -62,7 +62,7 @@ describe "Renderer" do
 
       renderer.board.clear(1, 1)
 
-      expected_output = /. . . \n. 0 . \n. . . \n/
+      expected_output = /0 1 2 \n\n. . . \t0\n. 0 . \t1\n. . . \t2\n/
       expect { renderer.draw_grid }.to output(expected_output).to_stdout
     end
 
@@ -71,7 +71,7 @@ describe "Renderer" do
 
       renderer.board.grid[1][1].flag
 
-      expected_output = /. . . \n. F . \n. . . \n/
+      expected_output = /0 1 2 \n\n. . . \t0\n. F . \t1\n. . . \t2\n/
       expect { renderer.draw_grid }.to output(expected_output).to_stdout
     end
 
@@ -79,7 +79,7 @@ describe "Renderer" do
       test_board.grid[1][1].mine = true
       renderer.board = test_board
 
-      expected_output = /. . . \n. . . \n. . . \n/
+      expected_output = /0 1 2 \n\n. . . \t0\n. . . \t1\n. . . \t2\n/
       expect { renderer.draw_grid }.to output(expected_output).to_stdout
     end
 
@@ -92,7 +92,7 @@ describe "Renderer" do
         
         renderer.board = test_board
 
-        expected_output = /X . . \n. X . \n. . X \n/
+        expected_output = /0 1 2 \n\nX . . \t0\n. X . \t1\n. . X \t2\n/
         expect { renderer.draw_grid(show_mines = true) }.to output(expected_output).to_stdout
       end
 
@@ -106,7 +106,7 @@ describe "Renderer" do
 
         renderer.board = test_board
 
-        expected_output = /X 2 1 \n. X . \n. . X \n/
+        expected_output = /0 1 2 \n\nX 2 1 \t0\n. X . \t1\n. . X \t2\n/
         expect { renderer.draw_grid(show_mines = true) }.to output(expected_output).to_stdout
       end
     end
