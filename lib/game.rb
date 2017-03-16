c6a9798b2473c6d33038ec99aa6a9127dda165b8
Roy Chen <roychenlei@gmail.com>
2017-03-16 13:45:45 +0800

    corrected error in instructions

--- lib/game.rb
+++ lib/game.rb
@@ -19,7 +19,7 @@ module Minesweeper
       puts "You might have played this game before, but here's a refresher:"
       puts "Every turn, you can choose to either clear, flag, or unflag a cell."
       puts "Please choose your action, then enter a coordinate in the format 'row, col'."
-      puts "Valid row values range from 0 to #{board.rows}, and valid column values range from 0 to #{board.cols}."
+      puts "Valid row values range from 0 to #{board.rows - 1}, and valid column values range from 0 to #{board.cols - 1}."
       puts "You win if you successfully clear all cells without clearing a mine."
       puts "Good luck!"
       puts
