f6e71a3673f86bb4472617a3a50391756cda47fd
Chad Lucas <chad.lucas.19@gmail.com>
2017-04-10 04:14:20 -0500

    add render method test

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,24 @@
 class Board 
+
+	attr_reader :board_arr
+
+	def initialize( boar_arr = nil)
+		#set up blank data structure
+		@board_arr = Array.new(10) {Array.new(10)}
+	end
+
+	def render
+		puts
+		@board_arr.each do |row|
+			row.each do |cell|
+				#display an existing marker if any, else blank
+				cell.nil? ? print("-") : print(cell.to_s)
+			end
+			puts
+		end
+		puts
+	end
 end
+
+b = Board.new
+b.render
