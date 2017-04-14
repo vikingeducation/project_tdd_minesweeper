c756960ad793385e003a6b8ffa3a505d6f0931d3
Sean Luckett <whitecrane007@gmail.com>
2017-04-14 17:06:43 -0600

    Create board and cells
    This starts development by creating a board class. This class fills a mine field with cells. The cells keep track of their coordinates. I picked a coordinate system for the array of cells because I have a feeling it'll come in handy when determining the mine count for each cell.

---
+++ lib/cell.rb
@@ -0,0 +1,15 @@
+class Cell
+  def initialize(row:, col:)
+    @row_coordinate = row
+    @column_coordinate = col
+    @contents = ' '
+  end
+
+  def render
+    @contents
+  end
+
+  def coordinates
+    {x: @row_coordinate, y: @column_coordinate}
+  end
+end
