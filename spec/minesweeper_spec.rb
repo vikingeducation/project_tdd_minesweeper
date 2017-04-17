cda1b02265366d254fcc59dc450c35ae7b98f8a0
Sean Luckett <whitecrane007@gmail.com>
2017-04-17 12:21:25 -0600

    Change flow of control to error-based
    Rather than look for valid inputs and call interaction methods on the UI
    class, I've decided to use errors for the particular inputs and have
    error classes handle which messages to display to player.

--- spec/minesweeper_spec.rb
+++ spec/minesweeper_spec.rb
@@ -7,29 +7,32 @@ RSpec.describe MineSweeper do
     context 'valid move/action' do
       before do
         allow(board_spy).to receive(:valid_move?) { true }
+        allow(minesweeper).to receive(:make_move)
         minesweeper.play
+        expect(minesweeper).to have_received(:make_move)
       end
 
-      it 'checks if cell is available' do
+      it 'displays the board' do
+        expect(ui_spy).to have_received(:display_board).with board_spy
+      end
+
+      it 'makes the move' do
         expect(ui_spy).to have_received(:get_cell_choice)
       end
 
       it 'prompts user for action on that cell' do
         expect(ui_spy).to have_received(:get_cell_action)
       end
-
-      it 'runs action on the cell' do
-        expect(board_spy).to have_received(:make_move)
-      end
     end
 
     context 'invalid move' do
       it 'displays feedback and prompts player for move' do
         allow(board_spy).to receive(:valid_move?).and_return(false, true)
+        allow(ui_spy).to receive(:get_cell_action).and_return('C', 'c')
 
-        minesweeper.play
-
-        expect(ui_spy).to have_received(:invalid_move)
+        expect { minesweeper.play }
+          .to output("Those coordinates are incorrect. Start again.\n")
+                .to_stdout
         expect(ui_spy).to have_received(:get_cell_choice).twice
       end
     end
@@ -37,11 +40,12 @@ RSpec.describe MineSweeper do
     context 'invalid action on cell' do
       it 'displays feedback and prompts player for action' do
         allow(board_spy).to receive(:valid_move?) { true }
-        allow(board_spy).to receive(:valid_action?).and_return(false, true)
+        allow(ui_spy).to receive(:get_cell_action).and_return('derp', 'c')
 
-        minesweeper.play
+        expect { minesweeper.play }
+          .to output("That action is illegal. Start again.\n")
+                .to_stdout
 
-        expect(ui_spy).to have_received(:invalid_action)
         expect(ui_spy).to have_received(:get_cell_action).twice
       end
     end
