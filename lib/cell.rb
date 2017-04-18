38312ed07e61329f066d0acbd15ab0fe0a401010
Sean Luckett <whitecrane007@gmail.com>
2017-04-18 14:21:07 -0600

    Have cell keep track of mine count and display it when cleared

--- lib/cell.rb
+++ lib/cell.rb
@@ -11,26 +11,30 @@ class Cell
     @neighbors = []
   end
 
-  def cleared?
-    @cleared
-  end
-
   def clear
     raise Errors::CellWasMinedError, 'You found a mine!' if mined?
 
     @cleared = true
-    @contents = ' '
+    @contents = mine_count > 0 ? mine_count.to_s : ' '
   end
 
-  def set_mine
-    @mined = true
+  def cleared?
+    @cleared
   end
 
   def coordinates
     { x: @row_coordinate, y: @column_coordinate }
   end
 
+  def mine_count
+    @count ||= @neighbors.collect { |cell| cell.mined? ? 1 : 0 }.inject(:+)
+  end
+
   def mined?
     @mined
+  end
+
+  def set_mine
+    @mined = true
   end
 end
