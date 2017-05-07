9915f646516095590a5d629c14d784a629eac2b2
Sean Kelly <shonkelli@gmail.com>
2017-05-07 15:55:15 -0400

    initialized guardfile

---
+++ lib/board.rb
@@ -0,0 +1,15 @@
+# board.rb
+class Board
+
+  # initialize
+  def initialize
+  # creates an array of arrays containing cells
+
+  # randomly assigns mines to cells
+
+
+  end
+
+  #returns number of mines
+
+end
---
+++ spec/board_spec.rb
@@ -0,0 +1,6 @@
+require 'rspec'
+require 'board'
+
+describe Board do
+  describe '#initialize'
+end
