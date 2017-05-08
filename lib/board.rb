47b5c72684fe06689791ca53d14ae7cc41b829a8
Sean Kelly <shonkelli@gmail.com>
2017-05-07 20:56:17 -0400

    completed most of the board-related code, will finish render class now

--- lib/board.rb
+++ lib/board.rb
@@ -1,15 +1,114 @@
 # board.rb
-class Board
+require_relative 'cell'
 
+class Board
+  attr_accessor :board_a, :num_flags
+  #returns number of mines
+  attr_reader :num_mines, :size
   # initialize
-  def initialize
-  # creates an array of arrays containing cells
+  def initialize(size = 10, num_mines = 9, num_flags = 9)
+    # creates an array of arrays containing cells
+    @size = size
+    @board_a = Array.new(size) {Array.new(size) {Cell.new}}
+    @num_mines = num_mines
+    @num_flags = num_flags
+    assign_mines
+  end
 
   # randomly assigns mines to cells
+  def assign_mines
+    randomizer.each do |num|
+      crd = coordinator(num)
+      cell = @board_a[crd[0]][crd[1]]
+      cell.plant_mine
+    end
+  end
+
+  #generates an array of randomly selected coordinates
+  def randomizer
+    output = []
+    max = (@size * @size) - 1
+    i = 0
+    while i < @num_mines
+       num = Random.new.rand(max)
+       unless output.include?(num)
+         output << num
+         i += 1
+       end
+    end
+    output.sort
+  end
+
+  #This handy tool turns an integer into x, y coordinates for the
+  def coordinator(num)
+    if num < 10
+      [0, num]
+    else
+      num.to_s.split('').map(&:to_i)
+    end
+  end
+
+end
+
+class Cell
+  attr_accessor :cleared
+  attr_reader :mine, :marked, :near_mines
+  #initializes a blank cell
+  def initialize
+    @mine = false
+    @marked = false
+    @cleared = false
+    @near_mines = 0
+  end
+
+  def plant_mine
+    @mine = true
+  end
+
+  def mark_mine
+    @marked = true
+  end
+
+end
+
+class Renderer
+  def initialize(board)
+    @board_a = board.board_a
+    @size = board.size
+  end
+
+  def render_board
+    # The size of the board
+    range = (0..(@size -1))
+    #
+    line_brk = "  "
+    @size.times {line_brk << "----"}
+    top = "   "
+    range.each {|e| top << "#{e}   "}
+    puts top
+    puts line_brk
+    range.each do |row|
+      new_row = "#{row}|"
+      range.each do |col|
+        # value =
+        new_row << " #{row} |"
+      end
+      puts new_row
+      puts line_brk
+
+    end
+  end
+
+  # get_symbol
+  def get_symbol(cell)
+    #analyes the cell value and returns the appropriate string
+
+
+
 
 
   end
-
-  #returns number of mines
-
 end
+
+r = Renderer.new(Board.new)
+r.render_board
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,5 +2,118 @@ require 'rspec'
 require 'board'
 
 describe Board do
-  describe '#initialize'
+  b = Board.new
+
+  describe '#initialize' do
+    it 'creates a board' do
+      expect(b).to be_a(Board)
+    end
+    it 'creates an array of 10 arrays by default' do
+      expect(b.board_a.length).to eq(10)
+      expect(b.board_a[0]).to be_a(Array)
+    end
+
+    it 'can create other size boards' do
+      b11 = Board.new(size = 11)
+      expect(b11.board_a.length).to eq(11)
+    end
+
+    it 'creates an array of arrays of cells' do
+      expect(b.board_a[0][1]).to be_a(Cell)
+    end
+
+    it 'contains 9 mines by default' do
+      expect(b.num_mines).to eq(9)
+    end
+
+    it 'should show remaining flags' do
+      expect(b.num_flags).to eq(9)
+    end
+  end
+
+  describe '#randomizer' do
+    it 'generates an array' do
+      expect(b.randomizer).to be_a(Array)
+    end
+    it 'generates an array with no duplicates' do
+      #This code checks to see if there are any duplicates
+      big_board = Board.new(size = 10, num_mines = 9)
+      orig = big_board.randomizer.length
+      no_dups = big_board.randomizer.uniq.length
+      expect(orig).to eq(no_dups)
+    end
+    it 'generates an array equal in length to the number of mines' do
+      b99 = Board.new(10, 99)
+      expect(b99.randomizer.length).to eq(99)
+    end
+  end
+
+  describe '#coordinator' do
+    #Note: this only works for arrays for 10X10 and smaller
+    it 'converts an integer into an array of two single digits' do
+      expect(b.coordinator(1)).to eq([0,1])
+      expect(b.coordinator(25)).to eq([2,5])
+    end
+  end
+
+  describe '#assign_mines' do
+    it 'calls randomizer' do
+      allow(b).to receive(:randomizer).and_return([1,2,3])
+      expect(b).to receive(:randomizer)
+      b.assign_mines
+    end
+    it 'updates the @mine of different cells' do
+      expect(b.assign_mines).to receive(:plant_mine)
+    end
+  end
 end
+
+describe Cell do
+  c = Cell.new
+
+  describe '#initialize' do
+    it 'creates a cell' do
+      expect(c).to be_a(Cell)
+    end
+    it 'does not have a mine by default' do
+      expect(c.mine).to eq(false)
+    end
+    it 'is not marked by default' do
+      expect(c.marked).to eq(false)
+    end
+    it 'is not cleared by default' do
+      expect(c.cleared).to eq(false)
+    end
+    it 'is not near any mines by default' do
+      expect(c.near_mines).to eq(0)
+    end
+
+    describe '#plant_mine' do
+      it 'plants a mine' do
+        expect(c.mine).to eq(false)
+        c.plant_mine
+        expect(c.mine).to eq(true)
+      end
+    end
+
+    describe '#mark_mine' do
+      it 'marks the cell as having a mine' do
+        expect(c.marked).to eq(false)
+        c.mark_mine
+        expect(c.marked).to eq(true)
+      end
+    end
+  end
+end
+
+r = Renderer.new(Board.new)
+r.render_board
+# describe Renderer do
+#   r = Renderer.new(Board.new)
+#   describe '#render_board' do
+#     it 'outputs a string to the command line' do
+#       expect(r).to receive(:puts)
+#       r.render_board
+#     end
+#   end
+# end
