c55b84a1298469f9029ab68d7be2074fe9c0de02
Alyss Noland <alyss.noland@gmail.com>
2017-08-08 22:28:32 -0500

    Update to tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 describe Board do
 
   let(:board) { described_class.new(4, 4, 5) }
+  let(:none) { described_class.new(2, 2, 0) }
 
   describe '#initialize' do
     it 'has 5 mines' do
@@ -12,12 +13,18 @@ describe Board do
   end
 
   describe '#print_board' do
-    it 'something'
+    it 'prints a string that contains "?"' do
+      expect(board.print_board).to include("?")
+    end
+
+    it 'has no bombs in the printed board' do
+      expect(none.print_board).not_to include("B")
+    end
   end
 
   describe '#click' do
     it 'returns "mine"' do 
-      expect(board.click(2, 2)).to eql('mine')
+      expect(board.click(2, 2)).to change(@tiles[2][2].revealed)
     end
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,13 +4,18 @@ require 'spec_helper'
 describe Game do
 
   let(:game) { described_class.new(4, 4, 5) }
+  let(:ded) { described_class.new(2, 2, 5) }
 
-  describe '#click' do
-    it 'something'
+  describe '#intialize' do
+    it 'fails to intialize when too many bombs are entered' do
+      expect{ ded }.to raise_error(RuntimeError)
+    end
   end
 
-  describe '#flag' do
-    it 'something'
+  describe '#click' do
+    it 'returns information about the game ending'
+
+    it 'tells the user the tile had no bomb and has been revealed'
   end
 
 end
