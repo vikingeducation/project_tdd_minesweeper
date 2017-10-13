595ea807a4b72622dbf861443d4ac623a116e709
anne <richardson.ae@gmail.com>
2017-10-13 16:41:26 -0500

    Code and specs for Cell

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,11 +1,29 @@
+require_relative 'icon'
+
 class Cell
+  include Icon
+
   attr_accessor :value
+
   def initialize
     @value = 0
   end
 
+  def revealed?
+    false
+  end
+
+  def to_s
+    "#{value} "
+  end
+
+  def flag
+    "#{Icon::FLAG} "
+  end
 end
 
 class Mine < Cell
-
+  def to_s
+    "#{Icon::MINE} "
+  end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,5 +1,41 @@
 require 'cell'
+require 'icon'
 
 describe Cell do
+  include Icon
+
+  let(:cell) { Cell.new }
+
+  describe '#initialize' do
+    it("is a Cell") { expect(cell).to be_a(Cell) }
+    it("has a default value of 0") { expect(cell.value).to eq(0) }
+    it("is hidden by default") { expect(cell.revealed?).to eq(false) }
+  end
+
+  describe '#to_s' do
+    it 'displays the value of the cell' do
+      cell.value = 3
+      expect(cell.to_s.to_i).to eq(3)
+    end
+  end
+
+  describe '#flag' do
+    it 'displays a flag icon + a space' do
+      expect(cell.flag).to eq("#{Icon::FLAG} ")
+    end
+  end
+
+end
+
+describe Mine do
+  let(:mine) { Mine.new }
+
+  it("is a subclass of Cell") { expect(mine).to be_a(Cell) }
+
+  describe '#to_s' do
+    it 'displays a mine icon + a space' do
+      expect(mine.to_s).to eq("#{Icon::MINE} ")
+    end
+  end
 
 end
